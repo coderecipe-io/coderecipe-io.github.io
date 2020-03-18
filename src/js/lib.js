1ff48bd8c3279100e70c6965b9619acb204c6980
dvmoomoodv <dvmoomoodv@gmail.com>
2020-03-19 00:56:47 +0900

    css webpack build 환경 구축

---
+++ src/js/lib.js
@@ -0,0 +1,14 @@
+export const pi = Math.PI;
+export const power = (x, y) => {
+    return x * y;
+}
+export class Foo{
+    foo(){
+        console.log('foo');
+        return 'foo';
+    }
+    bar(){
+        console.log('bar');
+        return 'bar';
+    }
+}
